0d2923518c58587939d7300cc246ca1f6ed3275b
Dustin K Greco <dkgreco@thetroveofgems.tech>
2024-12-09 13:54:49 -0700

    Add Domain Back

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -105,6 +105,7 @@ export const handleLogin = async (req, res, next) => {
 
   return res
     .cookie(process.env.APP_AUTH_COOKIE_NAME, token, {
+      domain: process.env.APP_AUTH_DOMAIN,
       path: process.env.APP_AUTH_PATH,
       httpOnly: true,
       secure: true,
