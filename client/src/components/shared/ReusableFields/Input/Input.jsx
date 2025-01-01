7238c61a310cc712548e548fbcb37b432a3b9b25
Dustin K Greco <dkgreco@thetroveofgems.tech>
2025-01-01 11:17:02 -0700

    Added MinMax Lengths to Input Fields For Login and Registration Forms

--- client/src/components/shared/ReusableFields/Input/Input.jsx
+++ client/src/components/shared/ReusableFields/Input/Input.jsx
@@ -1,81 +1,102 @@
-import React, {useState} from "react";
-import { findInputError } from '../../../../utils/findInputError';
-import { isFormInvalid } from '../../../../utils/isFormInvalid';
-import { useFormContext } from 'react-hook-form';
-import { AnimatePresence, motion } from 'framer-motion';
-import { MdError } from 'react-icons/md';
+import React, { useState } from "react";
+import { findInputError } from "../../../../utils/findInputError";
+import { isFormInvalid } from "../../../../utils/isFormInvalid";
+import { useFormContext } from "react-hook-form";
+import { AnimatePresence, motion } from "framer-motion";
+import { MdError } from "react-icons/md";
 
 import "./Input.css";
-import {DynamicIcon} from "../DynamicIcon/DynamicIcon";
+import { DynamicIcon } from "../DynamicIcon/DynamicIcon";
 export const Input = ({
-                          label, type, id, placeholder, validation,
-                          name, iconName,
-                          value = undefined, disabled = false,
-                          useOnChange = false, onChangeHandler = null
+  label,
+  type,
+  id,
+  placeholder,
+  validation,
+  name,
+  iconName,
+  minLength,
+  maxLength,
+  value = undefined,
+  disabled = false,
+  useOnChange = false,
+  onChangeHandler = null,
 }) => {
-    const
-        { register, formState: { errors, touchedFields, dirtyFields } } = useFormContext(),
-        inputError = findInputError(errors, name),
-        isInvalid = isFormInvalid(inputError);
+  const {
+      register,
+      formState: { errors, touchedFields, dirtyFields },
+    } = useFormContext(),
+    inputError = findInputError(errors, name),
+    isInvalid = isFormInvalid(inputError);
 
-    const handleChange = (id, evt) => {
-        console.log("Change? ", id, evt);
-    }
+  const handleChange = (id, evt) => {
+    console.log("Change? ", id, evt);
+  };
 
-    return <div className="input__container">
-        <div className="input__wrap">
-            <div className="input__label__container">
-                <label htmlFor={id} className="input__label">
-                    {label}
-                </label>
-            </div>
-            <div>
-                <AnimatePresence mode="wait" initial={false}>
-                    {isInvalid && (
-                        <InputError
-                            message={inputError.error.message}
-                            key={inputError.error.message}
-                        />
-                    )}
-                </AnimatePresence>
-            </div>
-            <div className={"expandableDiv"}></div>
+  return (
+    <div className="input__container">
+      <div className="input__wrap">
+        <div className="input__label__container">
+          <label htmlFor={id} className="input__label">
+            {label}
+          </label>
         </div>
-        <div className={"input__field__wrapper"}>
-            <DynamicIcon icon={iconName}/>
-            { value === undefined ? (<input
-                aria-autocomplete={"none"}
-                id={id}
-                type={type}
-                className="input__field"
-                placeholder={placeholder}
-                {...register(name, validation)}
-            />) : (<input
-                autoComplete={"off"}
-                aria-autocomplete={"none"}
-                id={id}
-                type={type}
-                className={`input__field ${disabled ? "disabled" : ""}`}
-                defaultValue={value}
-                disabled={disabled}
-                {...register(name, validation)}
-            />)}
+        <div>
+          <AnimatePresence mode="wait" initial={false}>
+            {isInvalid && (
+              <InputError
+                message={inputError.error.message}
+                key={inputError.error.message}
+              />
+            )}
+          </AnimatePresence>
         </div>
-    </div>;
-}
-export const InputError = ({message}) => {
-    return (
-        <motion.p
-            className="input__error"
-            {...{
-                initial: {opacity: 0, y: 10},
-                animate: {opacity: 1, y: 0},
-                exit: {opacity: 0, y: 10},
-                transition: {duration: 0.2}
-            }}
-        >
-            <MdError/>
-            <span className={"spanSpacer"}>{message}</span>
-        </motion.p>
-    );
+        <div className={"expandableDiv"}></div>
+      </div>
+      <div className={"input__field__wrapper"}>
+        <DynamicIcon icon={iconName} />
+        {value === undefined ? (
+          <input
+            aria-autocomplete={"none"}
+            id={id}
+            type={type}
+            className="input__field"
+            placeholder={placeholder}
+            minLength={minLength}
+            maxLength={maxLength}
+            {...register(name, validation)}
+          />
+        ) : (
+          <input
+            autoComplete={"off"}
+            aria-autocomplete={"none"}
+            id={id}
+            type={type}
+            className={`input__field ${disabled ? "disabled" : ""}`}
+            defaultValue={value}
+            minLength={minLength}
+            maxLength={maxLength}
+            disabled={disabled}
+            {...register(name, validation)}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+export const InputError = ({ message }) => {
+  return (
+    <motion.p
+      className="input__error"
+      {...{
+        initial: { opacity: 0, y: 10 },
+        animate: { opacity: 1, y: 0 },
+        exit: { opacity: 0, y: 10 },
+        transition: { duration: 0.2 },
+      }}
+    >
+      <MdError />
+      <span className={"spanSpacer"}>{message}</span>
+    </motion.p>
+  );
 };
